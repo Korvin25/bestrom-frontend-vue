3ae218160d4e163e4949f0b60d458b2a79164cdc
checkkick work <tuskancik226@gmail.com>
2021-12-08 15:33:25 +0300

    add post form

--- src/components/appModalMenuApplication.vue
+++ src/components/appModalMenuApplication.vue
@@ -17,32 +17,46 @@
 					<h3>8-800-555-74-57</h3>
 				</a>
 			</div>
-			<form class="form-call flex-column" action="">
+			<section class="form-call flex-column">
 				<label for="company">{{ $store.state.language === 'RU' ? 'Компания' : 'Company' }}</label>
 				<input
 					id="company"
+					v-model="inputCompany"
 					type="text"
 					class="input"
 					:placeholder="$store.state.language === 'RU' ? 'БЕСТРОМ' : 'BESTROM'" />
 				<label for="fio">{{ $store.state.language === 'RU' ? 'Ф.И.О' : 'Full name' }}</label>
 				<input
 					id="fio"
+					v-model="inputName"
 					type="text"
 					class="input"
 					:placeholder="$store.state.language === 'RU' ? 'Иван Иванович' : 'Ivan Ivanovich'" />
 				<label for="telephone">{{ $store.state.language === 'RU' ? 'Телефон' : 'Telephone' }}</label>
-				<input id="telephone" type="text" class="input" placeholder="89199966203" />
+				<input
+					id="telephone"
+					v-model="inputTelephone"
+					type="text"
+					class="input"
+					placeholder="89199966203" />
 				<label for="email">E-mail</label>
-				<input id="email" type="text" class="input" placeholder="partner@thedimension.com" />
+				<input
+					id="email"
+					v-model="inputEmail"
+					type="text"
+					class="input"
+					placeholder="partner@thedimension.com" />
 				<label for="product">{{ $store.state.language === 'RU' ? 'Продукт' : 'Product' }}</label>
 				<input
 					id="product"
+					v-model="inputProduct"
 					type="text"
 					class="input"
 					:placeholder="$store.state.language === 'RU' ? 'Фисташки' : 'Pistachio'" />
 				<label for="weight">{{ $store.state.language === 'RU' ? 'Дозировка' : 'Dosage' }}</label>
 				<input
 					id="weight"
+					v-model="inputDosage"
 					type="text"
 					class="input"
 					:placeholder="$store.state.language === 'RU' ? '100г' : '100g'" />
@@ -51,6 +65,7 @@
 				}}</label>
 				<input
 					id="speed"
+					v-model="inputPerformance"
 					type="text"
 					class="input"
 					:placeholder="$store.state.language === 'RU' ? '60 п/м' : '60 p/m'" />
@@ -76,18 +91,74 @@
 						<img src="../assets/mobile.png" alt="menu-item-img" />
 					</a>
 				</div>
-				<button class="call btn">
+				<button class="call btn" @click="sendPostContacts">
 					{{ $store.state.language === 'RU' ? 'ОТПРАВИТЬ ЗАЯВКУ' : 'SEND AN APPLICATION' }}
 				</button>
-			</form>
+			</section>
 		</div>
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 	name: 'AppModalMenuApplication',
 	emits: ['close'],
+	data() {
+		return {
+			inputCompany: '',
+			inputName: '',
+			inputTelephone: '',
+			inputEmail: '',
+			inputProduct: '',
+			inputDosage: '',
+			inputPerformance: '',
+		}
+	},
+	methods: {
+		sendPostContacts() {
+			if (
+				(this.inputTelephone.length > 10 ||
+					(this.inputEmail.includes('@') && this.inputEmail.length > 6)) &&
+				this.inputName.length !== 0 &&
+				this.inputProduct.length !== 0 &&
+				this.inputDosage.length !== 0 &&
+				this.inputPerformance.length !== 0
+			) {
+				axios
+					.post('http://bexram.online:8001/forms/', {
+						type: this.$store.state.language === 'RU' ? 'Заявка' : 'Application',
+						telephone: this.inputTelephone,
+						email: this.inputEmail,
+						name: this.inputName,
+						other:
+							(this.$store.state.language === 'RU' ? 'Компания: ' : 'Company: ') +
+							this.inputCompany +
+							(this.$store.state.language === 'RU' ? ', Продукт: ' : ', Product: ') +
+							this.inputProduct +
+							(this.$store.state.language === 'RU' ? ', Дозировка: ' : ', Dosage: ') +
+							this.inputDosage +
+							(this.$store.state.language === 'RU' ? ', Производительность: ' : ', Performance: ') +
+							this.inputPerformance,
+					})
+					.then(() => {
+						this.inputCompany = ''
+						this.inputTelephone = ''
+						this.inputName = ''
+						this.inputEmail = ''
+						this.inputProduct = ''
+						this.inputDosage = ''
+						this.inputPerformance = ''
+					})
+					.catch((error) => {
+						console.log(error)
+					})
+			} else {
+				alert('Проверьте правильность ввода всех полей!')
+			}
+		},
+	},
 }
 </script>
 
