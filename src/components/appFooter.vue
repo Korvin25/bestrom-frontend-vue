3e70843c69cacf1048b249a1a83c7e3777dc050a
antonkostin <antonkostin86@gmail.com>
2025-11-10 16:19:20 +0300

    add link politic_filr

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -9,17 +9,39 @@
 		</p>
 		<p><a href="tel:+78005557457">+7-800-555-74-57</a></p>
 		<!-- <p>{{ $store.state.language === 'RU' ? 'ПРЕСС-ЦЕНТР' : 'PRESS CENTER' }}</p> -->
+		<p class="politic" v-if="PAGE_ID[0] && PAGE_ID[0].link ">
+			<a :href="PAGE_ID[0].politic_filr" target="_blank">{{
+				$store.state.language === 'RU' ? 'Политика конфиденциальности' : 'Privacy policy'
+			}}</a>
+		</p>
 	</footer>
 	<!-- /.footer -->
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
 export default {
 	name: 'AppFooter',
+	computed: {
+		...mapGetters({
+			PAGE_ID: 'page/PAGE_ID',
+		}),
+	},
+	mounted() {
+		this.GET_PAGE_ID(1)
+	},
+	methods: {
+		...mapActions({
+			GET_PAGE_ID: 'page/GET_PAGE_ID',
+		}),
+	},
 }
 </script>
 
 <style scoped>
+.politic {
+	margin-left: auto;
+}
 .footer {
 	justify-content: flex-start;
 }
