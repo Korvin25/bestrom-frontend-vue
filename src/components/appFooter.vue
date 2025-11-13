221c389f3bc42886c8cd4f4397902e4de462d32b
antonkostin <antonkostin86@gmail.com>
2025-11-13 15:09:48 +0300

    fix

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -9,7 +9,7 @@
 		</p>
 		<p><a href="tel:+78005557457">+7-800-555-74-57</a></p>
 		<!-- <p>{{ $store.state.language === 'RU' ? 'ПРЕСС-ЦЕНТР' : 'PRESS CENTER' }}</p> -->
-		<p class="politic" v-if="PAGE_ID[0] && PAGE_ID[0].politic_filr ">
+		<p class="politic">
 			<a href="/politic" target="_blank">
 				{{ $store.state.language === 'RU' ? 'обработку персональных данных' : 'processing of personal data' }}
 			</a>
